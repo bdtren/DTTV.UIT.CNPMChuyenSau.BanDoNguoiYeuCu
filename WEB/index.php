69d6ca5bdb0b4af2e8f3bcadbc4d221ae5f3c2fc
Admin <15520161@gm.uit.edu.vn>
2018-05-12 01:29:38 +0700

    Thêm phân biệt cho 2 trang index
    - Thêm comment phân biệt index.php với index.html

--- WEB/index.php
+++ WEB/index.php
@@ -77,7 +77,7 @@
 
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-	<!--**********************
+	<!--********************** Đây là file index.php
 	Include  Javascript library
 	**********************-->
 	<script src="./sw-register.js"></script>
