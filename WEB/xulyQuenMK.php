ee0521efc439260b3889296136143427b517a8a2
vosivai <37525574+vosivai@users.noreply.github.com>
2018-05-18 08:31:32 +0700

    Xử lý quên mật khẩu

--- WEB/xulyQuenMK.php
+++ WEB/xulyQuenMK.php
@@ -12,57 +12,47 @@
         if (!$username || !$password || !$email || !$confirmpassword) 
         {
             echo '<script language="javascript"> alert("Vui lòng nhập đầy đủ thông tin!") </script>';
-            $conn = null;
+            $mysqli_close($conn);
         }
         else
         {
             if($password != $confirmpassword)
             {
                 echo '<script language="javascript"> alert("Mật khẩu phải giống nhau") </script>';
-                $conn = null;
+                mysqli_close($conn);
             }
             else
-            {
+            {                
                 //Kiểm tra tên đăng nhập email có đúng 
                 $sql = "SELECT TAIKHOAN.MATK AS TK FROM TAIKHOAN,KHACHHANG WHERE TAIKHOAN.MATK=KHACHHANG.MATK AND TENTK='$username' AND EMAIL='$email' ";
-                $stmt = $conn->prepare($sql);
-                //Thiết lập kiểu dữ liệu trả về
-                $stmt->setFetchMode(PDO::FETCH_ASSOC);
-                
-                if($stmt->execute())
+                $result = mysqli_query($conn, $sql);
+
+                if (mysqli_num_rows($result) == 0) 
                 {
-                    $count = $stmt->rowCount();
-                    $result = $stmt->fetchAll();
-                    foreach ($result as $row) 
-                    {
-                        $MATK = $row['TK'];
-                    }
-                    if ($count == 0) 
-                    {
-                        echo '<script language="javascript"> alert("Tên đăng nhập hoặc email không đúng") </script>';
-                        $conn = null;
-                    }
-                    else
-                    {
-                        // nếu mọi thứ hợp lệ thì đổi mật khẩu ở csdl
-                        $sql = "UPDATE TAIKHOAN SET MATKHAU = '$password' WHERE MATK='$MATK' ";
-                        // use exec() because no results are returned
-                        if($conn->exec($sql))
-                        {
-                            //echo '<script language="javascript"> alert("Đổi mật khẩu thành công") </script>';   
-                            echo '<script language="javascript">if(!alert("Đổi mật khẩu thành công")) document.location = "index.php" </script>';
-                            //header("Location: ../index.php");
-                        }  
-                        else
-                        {
-                            echo '<script language="javascript"> alert("không thể update mậtt khẩu") </script>'; 
-                        }              
-                    }
+                    echo '<script language="javascript"> alert("Tên đăng nhập hoặc email không đúng") </script>';
+                    //echo "Tên đăng nhập này không tồn tại. Vui lòng kiểm tra lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
+                    mysqli_close($conn);
                 }
                 else
                 {
-                    echo '<script language="javascript"> alert("Không thể lấy tên đăng nhập , email") </script>'; 
-                }       
+                    if($row = mysqli_fetch_assoc($result))
+                        $MATK = $row['TK'];
+
+                    // nếu mọi thứ hợp lệ thì đổi mật khẩu ở csdl
+                    $sql = "UPDATE TAIKHOAN SET MATKHAU = '$password' WHERE MATK='$MATK' ";
+                    // use exec() because no results are returned
+                    if(mysqli_query($conn, $sql))
+                    {
+                        //echo '<script language="javascript"> alert("Đổi mật khẩu thành công") </script>';   
+                        echo '<script language="javascript">if(!alert("Đổi mật khẩu thành công")) document.location = "index.php" </script>';
+                        //header("Location: ../index.php");
+                    }  
+                    else
+                    {
+                        echo '<script language="javascript"> alert("không thể update mật khẩu") </script>'; 
+                    }         
+                    
+                }
             }
         }   
     }
