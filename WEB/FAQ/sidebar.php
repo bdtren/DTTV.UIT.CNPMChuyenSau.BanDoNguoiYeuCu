284ca0984ef713ff2074f04b8bba07907cfc0f75
anhtuankg1997 <anhtuankg1997@gmail.com>
2018-05-23 09:34:02 +0700

    Chỉnh sửa trang câu hỏi thường gặp

---
+++ WEB/FAQ/sidebar.php
@@ -0,0 +1,45 @@
+    <nav class="col-md-3 bg-light sidebar">
+          <div class="sidebar-sticky">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+				<a class="nav-link active">
+                  <span data-feather="home" style="color: #FF06B7; font-weight: bolder; font-size: 25px;">
+                  <i class="fa fa-recycle" style="color: #FF0581"></i>  Câu hỏi thường gặp</span>
+                </a>
+              </li>
+				<!-- Phần câu hỏi -->
+			  <li class="nav-item">
+                <a class="nav-link sidebar-ques" href="FAQ.php?ques=1" >
+                  <i class="fa fa-question-circle"></i>  Love Market là gì?
+                </a>
+              </li>
+				
+			  <li class="nav-item">
+                <a class="nav-link sidebar-ques" href="FAQ.php?ques=2" >
+                  <i class="fa fa-question-circle"></i>  Cách đăng một tin(bán/mua) lên Love Market?
+                </a>
+              </li>
+				
+			 <li class="nav-item">
+                <a class="nav-link sidebar-ques" href="FAQ.php?ques=3" >
+                  <i class="fa fa-question-circle"></i>  Các quy định về đăng tin của Love Market?
+                </a>
+              </li>
+				
+			 <li class="nav-item">
+                <a class="nav-link sidebar-ques" href="FAQ.php?ques=4" >
+                  <i class="fa fa-question-circle"></i>  Làm sao để kiểm tra tình trạng của một tin?
+                </a>
+              </li>
+				
+			 <li class="nav-item">
+                <a class="nav-link sidebar-ques" href="FAQ.php?ques=5" >
+                  <i class="fa fa-question-circle"></i>  Chi phí cho việc tạo một tin đặc biệt(tin mới, tin hot, tin giảm giá, đẩy tin) trên Love Market là bao nhiêu?
+                </a>
+              </li>
+         
+				
+				
+            </ul>
+          </div>
+        </nav>
