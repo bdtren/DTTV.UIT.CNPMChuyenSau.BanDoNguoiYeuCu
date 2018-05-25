8ec1e9abc3d95a0ac84a1e2edfab139d633f6cb7
Admin <15520161@gm.uit.edu.vn>
2018-05-26 01:39:11 +0700

    Chỉnh sửa numpage.php
    Thêm "..." với những trang quá dài

--- WEB/danh-muc/numpage.php
+++ WEB/danh-muc/numpage.php
@@ -4,27 +4,121 @@
 	<?php
 		$z=1; 
 		$actNum = "";
-		while( $z <= CEIL(DemSanPham($DanhMuc)/6))
-		{
-			if($numpage==$z||($numpage==0&&$z==1)){
-				$actNum = "active";
-			}
-			else{
-				$actNum = "";
-			}
-			
-			if(!empty($DanhMuc))
+		$numOfPages=CEIL(DemSanPham($DanhMuc)/6);
+		//$numOfPages = 100;
+
+		$show = 5;
+		if($numOfPages<=6){
+			while( $z <= $numOfPages)
 			{
-				if($z==1)
-					$string = "category.php?DanhMuc="."$DanhMuc"."&Sort="."$Sort"."&SortType="."$SortType";
-				else
-					$string = "category.php?DanhMuc="."$DanhMuc"."&Sort="."$Sort"."&SortType="."$SortType"."&numpage="."$z";
-			}
-			else		
-				$string = "category.php?&Sort="."$Sort"."&SortType="."$SortType"."&numpage="."$z";
+				if($numpage==$z||($numpage==0&&$z==1)){
+					$actNum = "active";
+				}
+				else{
+					$actNum = "";
+				}
 				
-			echo "<li class='page-item $actNum'><a class='page-link' href='$string'>$z</a></li>";
-			$z++;
+				if(!empty($DanhMuc))
+				{
+					if($z==1)
+						$string = "category.php?DanhMuc="."$DanhMuc"."&Sort="."$Sort"."&SortType="."$SortType";
+					else
+						$string = "category.php?DanhMuc="."$DanhMuc"."&Sort="."$Sort"."&SortType="."$SortType"."&numpage="."$z";
+				}
+				else		
+					$string = "category.php?&Sort="."$Sort"."&SortType="."$SortType"."&numpage="."$z";
+					
+				echo "<li class='page-item $actNum'><a class='page-link' href='$string'>$z</a></li>";
+				$z++;
+			}
+		} else{
+			if($numpage<5){
+				while( $z <= 5)
+				{
+					if($numpage==$z||($numpage==0&&$z==1)){
+						$actNum = "active";
+					}
+					else{
+						$actNum = "";
+					}
+					
+					if(!empty($DanhMuc))
+					{
+						if($z==1)
+							$string = "category.php?DanhMuc="."$DanhMuc"."&Sort="."$Sort"."&SortType="."$SortType";
+						else
+							$string = "category.php?DanhMuc="."$DanhMuc"."&Sort="."$Sort"."&SortType="."$SortType"."&numpage="."$z";
+					}
+					else		
+						$string = "category.php?&Sort="."$Sort"."&SortType="."$SortType"."&numpage="."$z";
+						
+					echo "<li class='page-item $actNum'><a class='page-link' href='$string'>$z</a></li>";
+					$z++;
+				}
+
+				//Xử lý trang cuối
+				echo "<li class='page-item'>...</li>";
+				$string = "category.php?DanhMuc="."$DanhMuc"."&Sort="."$Sort"."&SortType="."$SortType"."&numpage="."$numOfPages";
+				echo "<li class='page-item'><a class='page-link' href='$string'>$numOfPages</a></li>";
+			} else if($numpage>=5&&$numpage<=($numOfPages-5)){
+				//xử lý trang đầu tiên
+				$string = "category.php?DanhMuc="."$DanhMuc"."&Sort="."$Sort"."&SortType="."$SortType"."&numpage="."1";
+				echo "<li class='page-item'><a class='page-link' href='$string'>1</a></li>";
+				echo "<li class='page-item'>...</li>";
+
+				$z = $numpage-2;				
+				while($z<=($numpage+2)){
+					if($numpage==$z||($numpage==0&&$z==1)){
+						$actNum = "active";
+					}
+					else{
+						$actNum = "";
+					}
+					
+					if(!empty($DanhMuc))
+					{
+						$string = "category.php?DanhMuc="."$DanhMuc"."&Sort="."$Sort"."&SortType="."$SortType"."&n=1&numpage="."$z";
+					}
+					else		
+						$string = "category.php?&Sort="."$Sort"."&SortType="."$SortType"."&numpage="."$z";
+						
+					echo "<li class='page-item $actNum'><a class='page-link' href='$string'>$z</a></li>";
+					$z++;
+				}
+
+				//Xử lý trang cuối
+				echo "<li class='page-item'>...</li>";
+				$string = "category.php?DanhMuc="."$DanhMuc"."&Sort="."$Sort"."&SortType="."$SortType"."&numpage="."$numOfPages";
+				echo "<li class='page-item'><a class='page-link' href='$string'>$numOfPages</a></li>";
+			} else if($numpage>($numOfPages-5)){
+				//xử lý trang đầu tiên
+				$string = "category.php?DanhMuc="."$DanhMuc"."&Sort="."$Sort"."&SortType="."$SortType"."&numpage="."1";
+				echo "<li class='page-item'><a class='page-link' href='$string'>1</a></li>";
+				echo "<li class='page-item'>...</li>";
+
+				$z=$numOfPages-5;
+				while($z<=$numOfPages){
+					if($numpage==$z||($numpage==0&&$z==1)){
+						$actNum = "active";
+					}
+					else{
+						$actNum = "";
+					}
+					
+					if(!empty($DanhMuc))
+					{
+						if($z==1)
+							$string = "category.php?DanhMuc="."$DanhMuc"."&Sort="."$Sort"."&SortType="."$SortType";
+						else
+							$string = "category.php?DanhMuc="."$DanhMuc"."&Sort="."$Sort"."&SortType="."$SortType"."&numpage="."$z";
+					}
+					else		
+						$string = "category.php?&Sort="."$Sort"."&SortType="."$SortType"."&numpage="."$z";
+						
+					echo "<li class='page-item $actNum'><a class='page-link' href='$string'>$z</a></li>";
+					$z++;
+				}
+			}
 		}
 	
 	//<li class="page-item active"><a class="page-link" href="#">1</a></li>
