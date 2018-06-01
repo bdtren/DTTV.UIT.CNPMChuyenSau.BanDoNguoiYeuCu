525b0856e472024f9d624101d3e7c82571055d8e
Admin <15520161@gm.uit.edu.vn>
2018-06-02 04:40:37 +0700

    Chỉnh sửa report.php(80%)

--- WEB/js/script.report.js
+++ WEB/js/script.report.js
@@ -1,29 +1,51 @@
 //Xử lý cho lựa chọn thống kê
-var reportname = "";
+var reportname = "tin-dang";
 var sortType = "";
+var inputVal = "";
+var totalName = "";
+var unit = "";
+var chartName = "";
+
 
 function dropdownChangeEvent(selectedValue) {
   $("#gr-luachon").hide();
   $(".datetime-picker").hide();
   if (selectedValue == "tin-dang") {
     document.getElementById("gr-luachon").innerHTML =
-      '<label for="Choose Report" style="color:#E74C3C"> Thời gian:</label>\n<input id="rbngay" type="radio" name="rbtype" value="Daily">Ngày\n<input id="rbtuan" type="radio" name="rbtype" value="Weekly">Tuần\n<input id="rbthang" type="radio" name="rbtype" value="Monthly">Tháng\n<input id="rbnam" type="radio" name="rbtype" value="Yearly">Năm </h5>\n';
+      '<label for="Choose Report" style="color:#E74C3C"> Thời gian:</label>\n<input id="rbngay" type="radio" name="rbtype" value="Daily" checked>Ngày\n<input id="rbtuan" type="radio" name="rbtype" value="Weekly">Tuần\n<input id="rbthang" type="radio" name="rbtype" value="Monthly">Tháng\n<input id="rbnam" type="radio" name="rbtype" value="Yearly">Năm </h5>\n';
     reportname = "tin-dang";
+    totalName = "tin đăng";
+    unit = "tin";
+    chartName = "Biểu đồ Tin đăng";
   } else if (selectedValue == "nhan-vien") {
     document.getElementById("gr-luachon").innerHTML =
-      '<label for="Choose Report" style="color:#E74C3C"> Thời gian:</label>\n<input id="rbngay" type="radio" name="rbtype" value="Daily">Ngày\n<input id="rbthang" type="radio" name="rbtype" value="Monthly">Tháng\n<input id="rbquy" type="radio" name="rbtype" value="Quarterly">Quý\n<input id="rbnam" type="radio" name="rbtype" value="Yearly">Năm </h5>\n';
+      '<label for="Choose Report" style="color:#E74C3C"> Thời gian:</label>\n<input id="rbngay" type="radio" name="rbtype" value="Daily" checked>Ngày\n<input id="rbthang" type="radio" name="rbtype" value="Monthly">Tháng\n<input id="rbquy" type="radio" name="rbtype" value="Quarterly">Quý\n<input id="rbnam" type="radio" name="rbtype" value="Yearly">Năm </h5>\n';
     reportname = "nhan-vien";
+    totalName = "Lương";
+    unit = "VND";
+    chartName = "Biểu đồ Lương";    
   } else if (selectedValue == "thiet-bi") {
     document.getElementById("gr-luachon").innerHTML =
-      '<label for="Choose Report" style="color:#E74C3C"> Thời gian:</label>\n<input id="rbthang" type="radio" name="rbtype" value="Monthly">Tháng\n<input id="rbnam" type="radio" name="rbtype" value="Yearly">Năm </h5>\n';
+      '<label for="Choose Report" style="color:#E74C3C"> Thời gian:</label>\n<input id="rbthang" type="radio" name="rbtype" value="Monthly" checked>Tháng\n<input id="rbnam" type="radio" name="rbtype" value="Yearly">Năm </h5>\n';
     reportname = "thiet-bi";
+    totalName = "chi phí";
+    unit = "VND";
+    chartName = "Biểu đồ Chi phí cho thiết bị";    
   } else if (selectedValue == "doanh-thu") {
     document.getElementById("gr-luachon").innerHTML =
-      '<label for="Choose Report" style="color:#E74C3C"> Thời gian:</label>\n<input id="rbthang" type="radio" name="rbtype" value="Monthly">Tháng\n<input id="rbquy" type="radio" name="rbtype" value="Quarterly">Quý\n<input id="rbnam" type="radio" name="rbtype" value="Yearly">Năm </h5>\n';
+      '<label for="Choose Report" style="color:#E74C3C"> Thời gian:</label>\n<input id="rbthang" type="radio" name="rbtype" value="Monthly" checked>Tháng\n<input id="rbquy" type="radio" name="rbtype" value="Quarterly">Quý\n<input id="rbnam" type="radio" name="rbtype" value="Yearly">Năm </h5>\n';
     //Gọi bảng doanh thu
     reportname = "doanh-thu";
+    totalName = "Doanh thu";
+    unit = "VND";    
+    chartName = "Biểu đồ Doanh thu";        
   } else if (selectedValue == "thu-nhap") {
+    document.getElementById("gr-luachon").innerHTML =
+      '<label for="Choose Report" style="color:#E74C3C"> Thời gian:</label>\n<input id="rbthang" type="radio" name="rbtype" value="Monthly" checked>Tháng\n<input id="rbquy" type="radio" name="rbtype" value="Quarterly">Quý\n<input id="rbnam" type="radio" name="rbtype" value="Yearly">Năm </h5>\n';
     reportname = "thu-nhap";
+    totalName = "Thu nhập";
+    unit = "VND";    
+    chartName = "Biểu đồ Thu nhập";        
   }
   $("#gr-luachon").show();
 }
@@ -31,51 +53,58 @@ function dropdownChangeEvent(selectedValue) {
 function radiobtnChangeEvent() {
   //////////Radio thời gian
   $('input[type="radio"]').click(function() {
-    $(".datetime-picker").hide();
-    if (
-      document.getElementById("rbngay") != null &&
-      document.getElementById("rbngay").checked
-    ) {
-      document.getElementById("time-input").type = "date";
-      document.getElementById("time-input").placeholder = "Ngày";
-      sortType = "date";
-    } else if (
-      document.getElementById("rbtuan") != null &&
-      document.getElementById("rbtuan").checked
-    ) {
-      document.getElementById("time-input").type = "week";
-      document.getElementById("time-input").placeholder = "Tuần";
-      sortType = "week";
-    } else if (
-      document.getElementById("rbthang") != null &&
-      document.getElementById("rbthang").checked
-    ) {
-      document.getElementById("time-input").type = "month";
-      document.getElementById("time-input").placeholder = "Tháng";
-      sortType = "month";
-    } else if (
-      document.getElementById("rbquy") != null &&
-      document.getElementById("rbquy").checked
-    ) {
-      document.getElementById("time-input").type = "text";
-      document.getElementById("time-input").placeholder =
-        "Quý(Vd: Quý 1 năm 2018->1/2018)";
-      sortType = "quarter";
-    } else if (
-      document.getElementById("rbnam") != null &&
-      document.getElementById("rbnam").checked
-    ) {
-      document.getElementById("time-input").type = "text";
-      document.getElementById("time-input").placeholder = "Năm (VD: 2018)";
-      sortType = "year";
-    }
-    $(".datetime-picker").show();
+    radiobtnChoice();
   });
+}
+
+function radiobtnChoice() {
+  $(".datetime-picker").hide();
+  if (
+    document.getElementById("rbngay") != null &&
+    document.getElementById("rbngay").checked
+  ) {
+    document.getElementById("time-input").type = "date";
+    document.getElementById("time-input").placeholder = "Ngày";
+    sortType = "date";
+  } else if (
+    document.getElementById("rbtuan") != null &&
+    document.getElementById("rbtuan").checked
+  ) {
+    document.getElementById("time-input").type = "week";
+    document.getElementById("time-input").placeholder = "Tuần";
+    sortType = "week";
+  } else if (
+    document.getElementById("rbthang") != null &&
+    document.getElementById("rbthang").checked
+  ) {
+    document.getElementById("time-input").type = "month";
+    document.getElementById("time-input").placeholder = "Tháng";
+    sortType = "month";
+  } else if (
+    document.getElementById("rbquy") != null &&
+    document.getElementById("rbquy").checked
+  ) {
+    document.getElementById("time-input").type = "text";
+    document.getElementById("time-input").placeholder =
+      "Quý(Vd: Quý 1 năm 2018->1/2018)";
+    sortType = "quarter";
+  } else if (
+    document.getElementById("rbnam") != null &&
+    document.getElementById("rbnam").checked
+  ) {
+    document.getElementById("time-input").type = "number";
+    document.getElementById("time-input").placeholder = "Năm (VD: 2018)";
+    //document.getElementById("time-input").min = "0";
+    // document.getElementById("time-input").pattern = "\d*";
+    // document.getElementById("time-input").step = "1";
+    sortType = "year";
+  }
+  $(".datetime-picker").show();
 }
 
 $(document).ready(function() {
   $(".datetime-picker").toggle();
-
+  radiobtnChoice();
   //////////Selection thống kê
   $("#thong-ke").change(function() {
     var optionSelected = document.getElementById("thong-ke");
@@ -83,6 +112,7 @@ $(document).ready(function() {
       optionSelected.options[optionSelected.selectedIndex].value;
 
     dropdownChangeEvent(optionSelected_value);
+    radiobtnChoice();
 
     //////////Radio thời gian
     radiobtnChangeEvent();
@@ -95,6 +125,25 @@ $(document).ready(function() {
 
   //////////Button View
   $("#btnView").click(function() {
+    //Kiểm tra thời gian đầu vào
+    var timeVal = document.getElementById("time-input").value;
+    alert(timeVal);
+    if (timeVal) {
+      // Kiểm tra cấu trúc đầu vào quý
+      if (document.getElementById("time-input").type == "text") {
+        const checkKey = /^[1-4]\/[0-9]{4}?$/;
+        if (!checkKey.test(timeVal)) {
+          alert(
+            "Lỗi nhập liệu: yêu cầu nhập đúng cấu trúc (quý)/(năm) với quý từ 1 đến 4"
+          );
+          document.getElementById("time-input").value = null;
+          return;
+        }
+      }
+
+      inputVal = timeVal.toString();
+    }
+
     if (reportname != "") {
       //Xóa datatable trước đó
       dataTable.destroy();
@@ -102,7 +151,7 @@ $(document).ready(function() {
 
       $.ajax({
         url: "./xulyphp/XuLyReport.php",
-        data: { callTable: reportname, sort: sortType },
+        data: { callTable: reportname, sort: sortType, inputValue: inputVal },
         type: "post",
         success: function(output) {
           var arr = output.split("//");
@@ -126,11 +175,13 @@ $(document).ready(function() {
 
           /**************************/
           //\/\/\/\/\Biểu đồ/\/\/\/\/\\
+          var sum = 0;
           var combine = new Array();
           var j = 1;
           combine[0] = [arr[1], arr[2]];
           for (var i = 3; i < arr.length - 1; i += 2) {
             combine[j] = [arr[i], parseInt(arr[i + 1])];
+            sum += parseInt(arr[i + 1]);
             j++;
           }
           // alert(combine);
@@ -141,14 +192,14 @@ $(document).ready(function() {
 
           var data = google.visualization.arrayToDataTable(combine, false);
           var formatter = new google.visualization.NumberFormat({
-            suffix: " VND"
+            suffix: " "+unit
           });
 
           formatter.format(data, 1);
 
           // Optional; add a title and set the width and height of the chart
           var options = {
-            title: "Biểu đồ Lương",
+            title: chartName,
             width: 550,
             height: 400
           };
@@ -159,7 +210,18 @@ $(document).ready(function() {
           );
           chart.draw(data, options);
 
-          //\/\/\/\/\ /\/\/\/\/\\
+          //\/\/\/\/\TÍnh tổng/\/\/\/\/\\
+          const numberWithCommas = (x) => {
+            var parts = x.toString().split(".");
+            parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            return parts.join(".") +" "+ unit;
+          }
+          var totalValue =
+            '<label id="total-label" style="color:#E74C3C" for="Total">Tổng ' +
+            totalName +
+            ": </label> " +
+            numberWithCommas(sum);
+          document.getElementById("total-count").innerHTML = totalValue;
         }
       });
     }
--- WEB/xulyphp/XuLyReport.php
+++ WEB/xulyphp/XuLyReport.php
@@ -1,30 +1,167 @@
 <?php 
 $action = "";
 $sortType = "";
+$inputValue = "";
+$query = "";
+$timeSelect = "NGAYDANG";
 
 //Thân hàm chính
 if(isset($_POST['callTable']) && !empty($_POST['callTable'])) {
     $action = $_POST['callTable'];
     if (isset($_POST['sort']) && !empty($_POST['sort'])){
         $sortType = $_POST['sort'];
+
+        if(isset($_POST['inputValue']) && !empty($_POST['inputValue'])){
+            $inputValue = $_POST['inputValue'];
+
+            switch($action) {
+                case 'tin-dang' : $timeSelect = "NGAYDANG"; break;
+                case 'nhan-vien': $timeSelect = "NGAYPC"; break;
+                case 'thiet-bi': $timeSelect = "NGAYKT"; break;
+                case 'doanh-thu' : $timeSelect = "NGAYTHU"; break;
+                case 'thu-nhap': $timeSelect = "NGAYPC"; break;
+                default: break;
+            }
+
+            switch($sortType){
+                case 'date':
+                    $query = ' and date('.$timeSelect.') BETWEEN "'.$inputValue.' 00:00:00" AND "'.$inputValue.' 23:59:59"';
+                    break;
+                case 'week':
+                    $dates = explode("-", $inputValue);
+                    $week = substr($dates[1], 1);
+                    $firstDayofYear = new DateTime(''.$dates[0].'-1-1');                  
+                    $firstMonday = $firstDayofYear->modify('second monday')->format('Y-m-d');
+        
+                    $step = ($week-3)*7;
+                    $thisMonday = date('Y-m-d', strtotime($firstMonday."+".$step." days"));
+                    $thisSunday = date('Y-m-d', strtotime($thisMonday."+6 days"));
+                    
+                    $query = ' and date('.$timeSelect.') BETWEEN "'.$thisMonday.' 00:00:00" AND "'.$thisSunday.' 23:59:59"';
+                    break;
+                case 'month':
+                    $dates = explode("-", $inputValue);
+                    $query = ' and month('.$timeSelect.')='.$dates[1].' and year('.$timeSelect.')='.$dates[0].'';
+                    break;
+                case 'quarter':
+                    $dates = explode("/", $inputValue);
+                    $groupMonth = "";
+                    switch($dates[0]){
+                        case 1: 
+                            $groupMonth = "(1, 2, 3)";
+                            break;
+                        case 2: 
+                            $groupMonth = "(4, 5, 6)";
+                            break;
+                        case 3: 
+                            $groupMonth = "(7, 8, 9)";
+                            break;
+                        case 4:
+                            $groupMonth = "(10, 11, 12)"; 
+                            break;
+                        default : 
+                            break;
+                    }
+                    $query = ' and month('.$timeSelect.') in '.$groupMonth.' and year('.$timeSelect.')='.$dates[1].''; 
+                    break;
+                case 'year':
+                $query = ' and year('.$timeSelect.')='.$inputValue.'';
+                    break;
+                default: 
+                    break;
+            }
+        }
     }
+    
     switch($action) {
-        case 'tin-dang' : bangTinDang($sortType); break;
-        case 'nhan-vien': bangNhanVien($sortType); break;
-        case 'thiet-bi': bangThietBi($sortType); break;
-        case 'doanh-thu' : bangDoanhThu($sortType);break;
-        case 'thu-nhap': bangThuNhap($sortType); break;
+        case 'tin-dang': bangTinDang($query); break;
+        case 'nhan-vien': bangNhanVien($query); break;
+        case 'thiet-bi': bangThietBi($query); break;
+        case 'doanh-thu' : bangDoanhThu($query); break;
+        case 'thu-nhap': bangThuNhap($query); break;
         default: break;
-        // ...etc...
     }
+
+    
 }
 
 ///\/\/\/\/\/\/\/CÁC HÀM XỬ LÝ GỌI CSDL\/\/\/\/\/\/\/\/\\\
-function csdlNhanVien(){
+function csdlTinDang($sort=""){
     $a = null;
     include('../xulyphp/connect.php');
-    $sql="select nv.MANV, HOTEN, TENCV, SOGIOHD, HSLUONG, (cv.LUONGCB*cv.HSLUONG*pc.SOGIOHD) LUONG from nhanvien nv, chucvu cv, phancong pc
-    where nv.MANV = pc.MANV and nv.MACV=cv.MACV;";
+    $sql='select cttd.MADM, TENDM, count(MATD) SoTin 
+    from danhmuc dm, (select MADM, td.MATD, TIEUDE, NGAYDANG 
+                        from tindang td, td_thuoc_dm tddm
+                        where td.MATD=tddm.MATD'.$sort.') cttd
+    where dm.MADM = cttd.MADM
+    group by cttd.MADM
+    order by cttd.MADM asc;';
+
+    mysqli_set_charset($conn, "utf8");
+        if($result = mysqli_query($conn, $sql))
+        {
+            while($row = mysqli_fetch_assoc($result))
+            {
+                $a[] = $row;
+            }
+        }
+        mysqli_close($conn);
+        return $a;
+}
+
+
+function csdlNhanVien($sort=""){
+    $a = null;
+    include('../xulyphp/connect.php');
+    $sql='select nv.MANV, HOTEN, TENCV, SOGIOHD, HSLUONG, (cv.LUONGCB*cv.HSLUONG*pc.SOGIOHD) Luong 
+    from nhanvien nv, chucvu cv, phancong pc
+    where nv.MANV = pc.MANV and nv.MACV=cv.MACV'.$sort.';';
+
+    mysqli_set_charset($conn, "utf8");
+        if($result = mysqli_query($conn, $sql))
+        {
+            while($row = mysqli_fetch_assoc($result))
+            {
+                $a[] = $row;
+            }
+        }
+        mysqli_close($conn);
+        return $a;
+}
+
+function csdlThietBi($sort=""){
+    $a = null;
+    include('../xulyphp/connect.php');
+    $sql='select tb.MATB, TENTB, GIATRI, NGAYNHAP, MAKT, NGAYKT, TINHTRANG, ChiPhi, kt.GHICHU
+    from thietbi tb, kiemtratb kt, (select tb1.MATB, case
+                                                when TINHTRANG="Hu" 
+                                                    then GIATRI
+                                                else 0
+                                            end as ChiPhi
+                                    from thietbi tb1, kiemtratb kt1
+                                    where tb1.MATB = kt1.MATB'.$sort.') sc
+    where tb.MATB = kt.MATB and sc.MATB = kt.MATB'.$sort.';';
+
+    mysqli_set_charset($conn, "utf8");
+        if($result = mysqli_query($conn, $sql))
+        {
+            while($row = mysqli_fetch_assoc($result))
+            {
+                $a[] = $row;
+            }
+        }
+        mysqli_close($conn);
+        return $a;
+}
+
+function csdlDoanhThu($sort=""){
+    $a = null;
+
+    include('../xulyphp/connect.php');
+    $sql='select MADT, LYDO, NGAYTHU, DOANHTHU 
+    from doanhthu 
+    where MADT is not NULL'.$sort.'
+    group by LYDO, NGAYTHU;';
 
     mysqli_set_charset($conn, "utf8");
         if($result = mysqli_query($conn, $sql))
@@ -40,38 +177,44 @@ function csdlNhanVien(){
 
 
 
+
 /********************************************************/
 ///\/\/\/\/\/\/\/CÁC HÀM XỬ LÝ XUẤT KẾT QUẢ VÀO WEB\/\/\/\/\/\/\/\/\\\
 function bangTinDang($sort=""){
-    echo '<thead>
-            <tr>
-                <th class="text-center" width="115px">STT</th>
-                <th class="text-center" width="115px">Tên danh mục</th>
-                <th class="text-center" width="115px">Số lượng tin</th>
-                <th class="text-center" width="115px">Ngày</th>
-                <th class="text-center" width="115px">Ghi chú</th>
-            /tr>
-          </thead>
-          <tbody>
-            <tr>
-                <td class="text-center" width="150px">1/1/1111</td>
-                <td class="text-center" width="150px">1 tỷ</td>
-                <td class="text-center" width="150px">500 triệu</span></td>
-                <td class="text-center" width="150px">500 triệu</span></td>
-                <td class="text-center" width="150px">abcxyz</span></td>
-            </tr>
-            <tr>
-                <td class="text-center" width="150px">2/2/2222</td>
-                <td class="text-center" width="150px">10 tỷ</td>
-                <td class="text-center" width="150px">500 triệu</span></td>
-                <td class="text-center" width="150px">9.5 tỷ</span></td>
-                <td class="text-center" width="150px">abcxyz</span></td>
-            </tr>
-           </tbody>';
+    $result =  '<thead <!--'.$sort.'-->>
+    <tr>
+        <th class="text-center" width="115px">Mã danh mục</th>
+        <th class="text-center" width="115px">Tên danh mục</th>
+        <th class="text-center" width="115px">Số tin đăng</th>
+    /tr>
+  </thead>
+  <tbody>';
+  $a=csdlTinDang($sort);
+  if(!$a){
+      echo $result;
+      return;
+  }
+  $lengthA = count($a);
+
+  for( $i=0; $i<$lengthA; $i++){
+      $result.='<tr>
+      <td class="text-center" width="150px">'.$a[$i]['MADM'].'</td>
+      <td class="text-center" width="150px">'.$a[$i]['TENDM'].'</td>
+      <td class="text-center" width="150px">'.$a[$i]['SoTin'].'</span></td>   
+  </tr>';
+  }
+  $result.='</tbody>//Tên danh mục//số tin đăng';
+  
+  for( $i=0; $i<$lengthA; $i++){
+      $result.='//'.$a[$i]['TENDM'].'//'.$a[$i]['SoTin'];
+  }
+
+  echo $result;  
 }
 
 function bangNhanVien($sort=""){
-    $result = '<thead>
+    // <!--'.$sort.'-->
+    $result = '<thead> 
     <tr>
         <th class="text-center" width="115px">Mã nhân viên</th>
         <th class="text-center" width="115px">Tên nhân viên</th>
@@ -82,7 +225,11 @@ function bangNhanVien($sort=""){
     /tr>
   </thead>
   <tbody>';
-    $a=csdlNhanVien();
+    $a=csdlNhanVien($sort);
+    if(!$a){
+        echo $result;
+        return;
+    }
     $lengthA = count($a);
 
     for( $i=0; $i<$lengthA; $i++){
@@ -92,20 +239,99 @@ function bangNhanVien($sort=""){
         <td class="text-center" width="150px">'.$a[$i]['TENCV'].'</span></td>
         <td class="text-center" width="150px">'.$a[$i]['SOGIOHD'].'</span></td>
         <td class="text-center" width="150px">'.$a[$i]['HSLUONG'].'</span></td>
-        <td class="text-center" width="150px">'.$a[$i]['LUONG'].'</span></td>
+        <td class="text-center" width="150px">'.$a[$i]['Luong'].'</span></td>
     </tr>';
     }
     $result.='</tbody>//Họ tên//Lương';
     
     for( $i=0; $i<$lengthA; $i++){
-        $result.='//'.$a[$i]['HOTEN'].'//'.$a[$i]['LUONG'];
+        $result.='//'.$a[$i]['HOTEN'].'//'.$a[$i]['Luong'];
     }
 
     echo $result;
-
 }
 
-function bangDoanhThu($sort ="not thing"){
+function bangThietBi($sort =""){
+    echo '<thead>
+    <tr>
+        <th class="text-center" width="115px">Mã thiết bị</th>
+        <th class="text-center" width="115px">Tên thiết bị</th>
+        <th class="text-center" width="115px">Giá trị</th>
+        <th class="text-center" width="115px">Ngày nhập</th>
+        <th class="text-center" width="115px">Mã kiểm tra</th>
+        <th class="text-center" width="115px">Ngày kiểm tra</th>
+        <th class="text-center" width="115px">Tình trạng</th>
+        <th class="text-center" width="115px">Chi phí</th>
+        <th class="text-center" width="115px">Ghi chú</th>
+    /tr>
+  </thead>
+  <tbody>'; 
+  $a=csdlThietBi($sort);
+  if(!$a){
+      echo $result;
+      return;
+  }
+  $lengthA = count($a);
+
+  for( $i=0; $i<$lengthA; $i++){
+      $result.='<tr>
+      <td class="text-center" width="150px">'.$a[$i]['MATB'].'</td>
+      <td class="text-center" width="150px">'.$a[$i]['TENTB'].'</td>
+      <td class="text-center" width="150px">'.$a[$i]['GIATRI'].'</span></td>
+      <td class="text-center" width="150px">'.$a[$i]['NGAYNHAP'].'</span></td>
+      <td class="text-center" width="150px">'.$a[$i]['MAKT'].'</span></td>
+      <td class="text-center" width="150px">'.$a[$i]['NGAYKT'].'</span></td>
+      <td class="text-center" width="150px">'.$a[$i]['TINHTRANG'].'</span></td>
+      <td class="text-center" width="150px">'.$a[$i]['ChiPhi'].'</span></td>
+      <td class="text-center" width="150px">'.$a[$i]['GHICHU'].'</span></td>
+      
+  </tr>';
+  }
+  $result.='</tbody>//Tên Thiết bị//Chi phí bảo dưỡng';
+  
+  for( $i=0; $i<$lengthA; $i++){
+      $result.='//'.$a[$i]['TENTB'].'//'.$a[$i]['ChiPhi'];
+  }
+
+  echo $result;
+}
+
+function bangDoanhThu($sort =""){
+    $result = '<thead <!--'.$sort.'-->>
+    <tr>
+        <th class="text-center" width="115px">Mã Doanh thu</th>
+        <th class="text-center" width="115px">Lý do</th>
+        <th class="text-center" width="115px">Ngày thu</th>
+        <th class="text-center" width="115px">Doanh thu</th>
+    /tr>
+  </thead>
+  <tbody>'; 
+
+  $a=csdlDoanhThu($sort);
+  if(!$a){
+      echo $result;
+      return;
+  }
+  $lengthA = count($a);
+
+  for( $i=0; $i<$lengthA; $i++){
+      $result.='<tr>
+      <td class="text-center" width="150px">'.$a[$i]['MADT'].'</td>
+      <td class="text-center" width="150px">'.$a[$i]['LYDO'].'</td>
+      <td class="text-center" width="150px">'.$a[$i]['NGAYTHU'].'</span></td>
+      <td class="text-center" width="150px">'.$a[$i]['DOANHTHU'].'</span></td>
+  </tr>';
+  }
+  $result.='</tbody>//Lý do//Doanh thu';
+  
+  for( $i=0; $i<$lengthA; $i++){
+      $result.='//'.date("d-m-Y", strtotime($a[$i]['NGAYTHU'])).' '.$a[$i]['LYDO'].'//'.$a[$i]['DOANHTHU'];
+  }
+
+  echo $result;
+}
+
+function bangThuNhap($sort ="not thing"){
     echo '<thead>
             <tr>
                 <th class="text-center" width="115px">Ngày tháng năm</th>
