505643abf80ea9709c287fa358522140e822fbc7
Admin <15520161@gm.uit.edu.vn>
2018-06-20 17:29:26 +0700

    Sửa giao diện report(phần thiếu)

--- WEB/report.php
+++ WEB/report.php
@@ -74,7 +74,7 @@
         <form class="form-signin">
           <div class="container-fluid">
             <div class="row">
-              <div class="col-md-12 col-md-offset-2">
+              <div class="col-md-12">
                 <div class="panel panel-default">
                   <div class="panel panel-primary">
                     <div class="text-center">
